9bbd2add47c195a346d22a6aaf147e9501cb3270
Minh-Quan Tran <Minh-Quan Tran>
2015-11-22 22:17:59 +0100

    using typescript

---
+++ src/ts/server.ts
@@ -0,0 +1,68 @@
+/// <reference path="../../typings/node/node.d.ts" />
+/// <reference path="../../typings/express/express.d.ts" />
+/// <reference path="../../typings/bluebird/bluebird.d.ts" />
+/// <reference path="../../typings/github-electron/github-electron.d.ts" />
+
+import http = require('http');
+import express = require('express');
+import querystring = require('querystring');
+import fs = require('fs');
+import Promise = require('bluebird');
+import ApiModule = require('./api');
+
+var Config = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
+var Api = new ApiModule.Api(Config);
+
+var app = express();
+
+app.set('host', process.env.HOST || (Config.server && Config.server.host) || '127.0.0.1');
+app.set('port', process.env.PORT || (Config.server && Config.server.port) || 0);
+app.set('x-powered-by', false);
+
+// Add Access-Control headers
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// Static servings
+app.use(express.static(__dirname + '/../app'));
+
+// API
+app.get("/api/books/:id([0-9]+)/download/:format([a-z0-9]+)", Api.downloadBookById);
+app.get("/api/books/:id([0-9]+)/download/:format([a-z0-9]+).([a-z0-9]+)", Api.downloadBookById);
+app.get("/api/books/", Api.getBooks);
+app.get("/api/books/page/:page([0-9]+)", Api.getBooks);
+app.get("/api/books/page/:page([0-9]+)/:limit([0-9]+)", Api.getBooks);
+app.get("/api/books/:id([0-9]+)", Api.getBookById);
+app.get("/api/books/:id([0-9]+)/cover.jpg", Api.coverUrl);
+app.get("/config.js", function (req, res) {
+    res.send("var Config = " + JSON.stringify({
+            server: Config.server
+        })
+    )
+});
+
+// Create server
+var server = http.createServer(app).listen(app.get('port'), app.get('host'), function () {
+    app.set('host', server.address().address);
+    app.set('port', server.address().port);
+    Config.server = {
+        host: app.get('host'),
+        port: app.get('port')
+    };
+
+    console.log('Express server listening on ', app.get('host'), app.get('port'))
+});
+
+// Electron app
+try {
+    // Reply config to view through IPC, because port can be random
+    import ipc = require('ipc');
+    ipc.on('config', function (event, arg) {
+        event.returnValue = Config;
+    });
+} catch (e) {
+
+}
