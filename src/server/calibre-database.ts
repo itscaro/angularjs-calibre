4c3bb0008da5d3a78eb95dc5b7a432a58d5c8346
Minh-Quan TRAN <account@itscaro.me>
2016-03-21 01:46:28 +0100

    correction api

--- src/server/calibre-database.ts
+++ src/server/calibre-database.ts
@@ -3,194 +3,196 @@ import Sequelize = require('sequelize');
 
 module CalibreDatabase {
 
-    export class DB {
-        private db;
-        Book;
-        Author;
-        BookAuthor;
-        Rating;
-        BookRating;
-        Language;
-        BookLanguage;
-        Tag;
-        BookTag;
-        Data;
-        constructor(dsn: string) {
-            this.db = new Sequelize('', '', '', {
-                //logging: false,
-                dialect: 'sqlite',
-                // SQLite only
-                storage: dsn
-            });
-        }
-
-        init() {
-            this.Book = this.db.define('book',
-                {
-                    uuid: {
-                        type: Sequelize.STRING
-                    },
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    title: Sequelize.STRING,
-                    has_cover: Sequelize.BOOLEAN,
-                    path: Sequelize.STRING
-                },
-                {
-                    timestamps: false,
-                    getterMethods: {
-                        coverUrl: function() {
-                            if (this.has_cover) {
-                                return '/api/books/' + this.id + '/cover.jpg'
-                            } else {
-                                return null;
-                            }
-                        }
-                    },
-                    setterMethods: {
-                    }
-                }
-            );
-
-            this.Author = this.db.define(
-                'author',
-                {
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    name: Sequelize.STRING,
-                    link: Sequelize.STRING
-                },
-                {
-                    timestamps: false
-                }
-            );
-
-            this.BookAuthor = this.db.define(
-                'books_authors_link',
-                {
-                    book: Sequelize.INTEGER,
-                    author: Sequelize.INTEGER
-                },
-                {
-                    freezeTableName: true,
-                    timestamps: false
-                }
-            );
-
-            this.Rating = this.db.define(
-                'rating',
-                {
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    rating: Sequelize.INTEGER
-                },
-                {
-                    timestamps: false
-                }
-            );
-
-            this.BookRating = this.db.define(
-                'books_ratings_link',
-                {
-                    book: Sequelize.INTEGER,
-                    rating: Sequelize.INTEGER
-                },
-                {
-                    freezeTableName: true,
-                    timestamps: false
-                }
-            );
-
-            this.Language = this.db.define(
-                'language',
-                {
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    lang_code: Sequelize.STRING
-                },
-                {
-                    timestamps: false
-                }
-            );
-
-            this.BookLanguage = this.db.define(
-                'books_languages_link',
-                {
-                    book: Sequelize.INTEGER,
-                    lang_code: Sequelize.INTEGER
-                },
-                {
-                    freezeTableName: true,
-                    timestamps: false
-                }
-            );
-
-            this.Tag = this.db.define(
-                'tags',
-                {
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    name: Sequelize.STRING
-                },
-                {
-                    timestamps: false
-                }
-            );
-
-            this.BookTag = this.db.define(
-                'books_tags_link',
-                {
-                    book: Sequelize.INTEGER,
-                    tag: Sequelize.INTEGER
-                },
-                {
-                    freezeTableName: true,
-                    timestamps: false
-                }
-            );
-
-            this.Data = this.db.define(
-                'data',
-                {
-                    id: {
-                        type: Sequelize.INTEGER,
-                        primaryKey: true
-                    },
-                    book: Sequelize.INTEGER,
-                    format: Sequelize.STRING,
-                    name: Sequelize.STRING,
-                    uncompressed_size: Sequelize.INTEGER
-                },
-                {
-                    timestamps: false
-                }
-            );
-
-            this.Book.belongsToMany(this.Author, { through: this.BookAuthor, foreignKey: 'book' });
-            this.Author.belongsToMany(this.Book, { through: this.BookAuthor, foreignKey: 'author' });
-
-            this.Book.belongsToMany(this.Rating, { through: this.BookRating, foreignKey: 'book' });
-            this.Rating.belongsToMany(this.Book, { through: this.BookRating, foreignKey: 'rating' });
-
-            this.Book.belongsToMany(this.Language, { through: this.BookLanguage, foreignKey: 'book' });
-            this.Language.belongsToMany(this.Book, { through: this.BookLanguage, foreignKey: 'lang_code' });
-
-            this.Book.belongsToMany(this.Tag, { through: this.BookTag, foreignKey: 'book' });
-            this.Tag.belongsToMany(this.Book, { through: this.BookTag, foreignKey: 'tag' });
-
-            this.Book.hasMany(this.Data, { foreignKey: 'book' });
-        }
+  export class DB {
+    private db;
+    public Book;
+    public Author;
+    public BookAuthor;
+    public Rating;
+    public BookRating;
+    public Language;
+    public BookLanguage;
+    public Tag;
+    public BookTag;
+    public Data;
+    
+    constructor(dsn: string) {
+      this.db = new Sequelize('', '', '', {
+        //logging: false,
+        dialect: 'sqlite',
+        // SQLite only
+        storage: dsn
+      });
+      this.init()
     }
+
+    init() {
+      this.Book = this.db.define('book',
+        {
+          uuid: {
+            type: Sequelize.STRING
+          },
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          title: Sequelize.STRING,
+          has_cover: Sequelize.BOOLEAN,
+          path: Sequelize.STRING
+        },
+        {
+          timestamps: false,
+          getterMethods: {
+            coverUrl: function() {
+              if (this.has_cover) {
+                return '/api/books/' + this.id + '/cover.jpg'
+              } else {
+                return null;
+              }
+            }
+          },
+          setterMethods: {
+          }
+        }
+      );
+
+      this.Author = this.db.define(
+        'author',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          name: Sequelize.STRING,
+          link: Sequelize.STRING
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      this.BookAuthor = this.db.define(
+        'books_authors_link',
+        {
+          book: Sequelize.INTEGER,
+          author: Sequelize.INTEGER
+        },
+        {
+          freezeTableName: true,
+          timestamps: false
+        }
+      );
+
+      this.Rating = this.db.define(
+        'rating',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          rating: Sequelize.INTEGER
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      this.BookRating = this.db.define(
+        'books_ratings_link',
+        {
+          book: Sequelize.INTEGER,
+          rating: Sequelize.INTEGER
+        },
+        {
+          freezeTableName: true,
+          timestamps: false
+        }
+      );
+
+      this.Language = this.db.define(
+        'language',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          lang_code: Sequelize.STRING
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      this.BookLanguage = this.db.define(
+        'books_languages_link',
+        {
+          book: Sequelize.INTEGER,
+          lang_code: Sequelize.INTEGER
+        },
+        {
+          freezeTableName: true,
+          timestamps: false
+        }
+      );
+
+      this.Tag = this.db.define(
+        'tags',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          name: Sequelize.STRING
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      this.BookTag = this.db.define(
+        'books_tags_link',
+        {
+          book: Sequelize.INTEGER,
+          tag: Sequelize.INTEGER
+        },
+        {
+          freezeTableName: true,
+          timestamps: false
+        }
+      );
+
+      this.Data = this.db.define(
+        'data',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+          },
+          book: Sequelize.INTEGER,
+          format: Sequelize.STRING,
+          name: Sequelize.STRING,
+          uncompressed_size: Sequelize.INTEGER
+        },
+        {
+          timestamps: false
+        }
+      );
+
+      this.Book.belongsToMany(this.Author, { through: this.BookAuthor, foreignKey: 'book' });
+      this.Author.belongsToMany(this.Book, { through: this.BookAuthor, foreignKey: 'author' });
+
+      this.Book.belongsToMany(this.Rating, { through: this.BookRating, foreignKey: 'book' });
+      this.Rating.belongsToMany(this.Book, { through: this.BookRating, foreignKey: 'rating' });
+
+      this.Book.belongsToMany(this.Language, { through: this.BookLanguage, foreignKey: 'book' });
+      this.Language.belongsToMany(this.Book, { through: this.BookLanguage, foreignKey: 'lang_code' });
+
+      this.Book.belongsToMany(this.Tag, { through: this.BookTag, foreignKey: 'book' });
+      this.Tag.belongsToMany(this.Book, { through: this.BookTag, foreignKey: 'tag' });
+
+      this.Book.hasMany(this.Data, { foreignKey: 'book' });
+    }
+  }
 
 
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,7 @@ import express = require('express');
 import querystring = require('querystring');
 import fs = require('fs');
 import Promise = require('bluebird');
-import ApiModule = require('./api');
+import {ApiModule} from './api';
 
 var Config = JSON.parse(fs.readFileSync(__dirname + '/../config.json').toString());
 var Api = new ApiModule.Api(Config);
@@ -15,15 +15,15 @@ app.set('port', process.env.PORT || (Config.server && Config.server.port) || 0);
 app.set('x-powered-by', false);
 
 // Add Access-Control headers
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
 // Static servings
 app.use(express.static(__dirname + '/../app'));
-
+app.set("API", Api)
 // API
 app.get("/api/books/:id([0-9]+)/download/:format([a-z0-9]+)", Api.downloadBookById);
 app.get("/api/books/:id([0-9]+)/download/:format([a-z0-9]+).([a-z0-9]+)", Api.downloadBookById);
@@ -32,31 +32,31 @@ app.get("/api/books/page/:page([0-9]+)", Api.getBooks);
 app.get("/api/books/page/:page([0-9]+)/:limit([0-9]+)", Api.getBooks);
 app.get("/api/books/:id([0-9]+)", Api.getBookById);
 app.get("/api/books/:id([0-9]+)/cover.jpg", Api.coverUrl);
-app.get("/config.js", function (req, res) {
-    res.send("var Config = " + JSON.stringify({
-            server: Config.server
-        })
-    )
+app.get("/config.js", function(req, res) {
+  res.send("var Config = " + JSON.stringify({
+    server: Config.server
+  })
+  )
 });
 
 // Create server
-var server = http.createServer(app).listen(app.get('port'), app.get('host'), function () {
-    app.set('host', server.address().address);
-    app.set('port', server.address().port);
-    Config.server = {
-        host: app.get('host'),
-        port: app.get('port')
-    };
+var server = http.createServer(app).listen(app.get('port'), app.get('host'), function() {
+  app.set('host', server.address().address);
+  app.set('port', server.address().port);
+  Config.server = {
+    host: app.get('host'),
+    port: app.get('port')
+  };
 
-    console.log('Express server listening on ', app.get('host'), app.get('port'))
+  console.log('Express server listening on ', app.get('host'), app.get('port'))
 });
 
 // Electron app
 try {
-    // Reply config to view through IPC, because port can be random
-    require('electron').on('config', function (event, arg) {
-        event.returnValue = Config;
-    });
+  // Reply config to view through IPC, because port can be random
+  require('electron').on('config', function(event, arg) {
+    event.returnValue = Config;
+  });
 } catch (e) {
-    console.warn("No module electron")
+  console.warn("No module electron")
 }
